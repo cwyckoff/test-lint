50303862a09472469de3b4753990452a0a1f4b70
Chris Wyckoff <cbwyckoff@gmail.com>
2014-05-30 12:09:28 -0600

    initial commit

---
+++ src/test_lint/core.clj
@@ -0,0 +1,6 @@
+(ns test-lint.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/test_lint/core_test.clj
@@ -0,0 +1,7 @@
+(ns test-lint.core-test
+  (:require [clojure.test :refer :all]
+            [test-lint.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
